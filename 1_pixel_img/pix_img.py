e553ce53bb69dd2b2cade677a2ae288f6d747b5b
Félix Ernesto Charry Pastrana <feecharrypa@unal.edu.co>
2025-01-12 22:16:59 -0600

    update codigo de pixelear img

--- 1_pixel_img/pix_img.py
+++ 1_pixel_img/pix_img.py
@@ -3,32 +3,33 @@ import os
 
 """
 Codigo para pixelear las imagenes ubicadas en la carpeta:
-    img_source_and_results
+    0_sources_and_results
 """
 
-folder_loc = '/Users/charrypastrana/Documents/github/Pixelear_Imagen_Python/'
+# MacOS
+folder_loc = '/Users/charrypastrana/Documents/github/'
+# Windows
+#folder_loc = '/Users/felix/iCloudDrive/Documents/github/'
 
-folder_loc = '/Users/felix/iCloudDrive/Documents/github/Pixelear_Imagen_Python/'
-folder_loc += '0_sources_and_results'
+folder_loc += 'Pixelear_Imagen_Python/0_sources_and_results'
+
 print('Folder Loc: ', folder_loc)
 os.chdir(folder_loc)
 
-files = ['IMG_7699.jpg', 'IMG_7700.jpg']
+files = ['DSC00295.png', 'IMG_7700.jpg']
 
 #file_name = 'DSC_0155.JPEG'
 for file_name in files: 
     img = Image.open(file_name)
-
     no_pixels = [8,16,32,64,128,256,512,1024] # No. de Pixels
 
     # Find the name without the extation (png or jpeg)
     point_loc = file_name.find('.')
-    file_name_without_extension = file_name[:point_loc]
+    file_name_wo_extension = file_name[:point_loc]
     extension = file_name[point_loc:]
 
     for i in no_pixels:
-        print('Number of Pixels: ', i)
+        print('File: ', file_name, 'and number of pixels: ', i)
         imgSmall = img.resize((i,i), resample=Image.BILINEAR)
         result = imgSmall.resize(img.size, Image.NEAREST)
-        result.save(file_name_without_extension + '_pixe_' + str(i) + extension)
-
+        result.save(file_name_wo_extension + '_pixe_' + str(i) + extension)
